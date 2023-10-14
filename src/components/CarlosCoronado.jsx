87cc7b8d10a36ffbfdd4c1fa5d81f19e552a2f70
cocarls <carloscoronadouribe@gmail.com>
2023-10-13 22:34:57 -0500

    gccjhfcjh

--- src/components/CarlosCoronado.jsx
+++ src/components/CarlosCoronado.jsx
@@ -1,6 +1,12 @@
 const CarlosCoronado = () => {
   return (
-    <div>CarlosCoronado</div>
+    <article className="">
+        <picture>
+            <img className="w-[300px] rounded-xl" src="/rick.jpg" alt="" />
+        </picture>
+        <h3>soy carlos</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi aspernatur tempora eaque nostrum, quas soluta repellendus aliquid, natus saepe neque, animi blanditiis mollitia. Dolor beatae repellat voluptates sed quidem voluptate.</p>
+    </article>
   )
 }
 export default CarlosCoronado
