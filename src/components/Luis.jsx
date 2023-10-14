8ac9102ceb12273557cd5141e23ded24c441d863
luis <luis111ferrivera@gmail.com>
2023-10-13 23:34:34 -0400

    luis

--- src/components/Luis.jsx
+++ src/components/Luis.jsx
@@ -1,6 +1,21 @@
 const Luis = () => {
   return (
-    <div>Luis</div>
-  )
-}
-export default Luis
+    <article className="grid justify-center w text-center max-w-[300px] bg-blue-950 rounded-xl overflow-hidden mb-7">
+      <header className="grid justify-center bg-green-100/50">
+        <img
+          className="border-4 border-yellow-500 rounded-xl p-1 my-auto"
+          src="/yo.jpg"
+          alt=""
+        />
+      </header>
+      <h3 className="font-bold">Luis Fernado Hinojosa Rivera</h3>
+      <div className="max-w-[300px]">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          assumenda.
+        </p>
+      </div>
+    </article>
+  );
+};
+export default Luis;
